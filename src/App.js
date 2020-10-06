04316045fe54991c3df03fa14bbd50b2c6f90fa1
Joao Pedro <joao.ricardo88@gmail.com>
2020-10-06 22:44:33 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Typography } from 'antd';
+import { Avatar } from 'antd';
+import SubMenu from 'antd/lib/menu/SubMenu';
+import { AppstoreOutlined } from '@ant-design/icons';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+
+
+const { Header, Footer, Sider, Content } = Layout;
+const { Title } = Typography;
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+      <Header style={{padding:10}}>
+           <Avatar style={{float:'right'}} shape="square" size="large" icon={<UserOutlined />} src='./test.png' />
+           <Title style={{color:'pink'}} level={5}>Tobias Zé Paulo</Title>
+      </Header>
+      <Layout>
+        <Sider>
+          <Menu
+              defaultSelectedKeys={['dashboard']}
+              mode="inline"
+          >
+            <Menu.Item>
+               Dashboard
+            </Menu.Item>
+            <SubMenu
+             key="sub1"
+             title={
+               <span>
+                  <AppstoreOutlined />
+                  <span>Store</span>
+               </span>
+             }
+            >
+              <Menu.ItemGroup key='Store' title='Store'>
+                <Menu.Item key='Porto'> Porto </Menu.Item>
+                <Menu.Item key='Lisbon'> Lisbon</Menu.Item>
+                
+              </Menu.ItemGroup>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout>
+        <Content style={{ padding: '0 50px' }}>
+      <Breadcrumb style={{ margin: '16px 0' }}>
+        <Breadcrumb.Item>Home</Breadcrumb.Item>
+        <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+      </Breadcrumb>
+      <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+        <Content style={{ padding: '0 24px', minHeight: 500 }}>Content</Content>
+      </Layout>
+    </Content>
+         <Footer style={{ textAlign: 'center' }}>Designed by JP. <b><br></br>The noob about React</b> </Footer>
+        </Layout>
+      </Layout>
+     </Layout>
     </div>
   );
 }
